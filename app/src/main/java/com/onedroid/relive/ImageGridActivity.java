1bfc22339e28893997cd75102f9095c5bac6ca18
karangeorge97 <karan9@illinois.edu>
2022-11-10 20:28:20 -0600

    Added ability to dummy add a photo , opens

--- app/src/main/java/com/onedroid/relive/ImageGridActivity.java
+++ app/src/main/java/com/onedroid/relive/ImageGridActivity.java
@@ -22,7 +22,7 @@ import com.onedroid.relive.databinding.ActivityImageGridBinding;
 import com.onedroid.relive.service.AccountService;
 
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ImageGridActivity extends AppCompatActivity {
@@ -33,31 +33,16 @@ public class ImageGridActivity extends AppCompatActivity {
     CustomAdapter customAdapter;
 
     GridView gridView;
-   List<Integer> images_selected;
+    List<Integer> images_selected = new ArrayList<>();
 
-   List<Integer>images_grad = Arrays.asList(R.drawable.m1_grad,R.drawable.m2_grad,R.drawable.m3_grad,R.drawable.m4_grad,
-            R.drawable.m5_grad,R.drawable.m6_grad,R.drawable.m7_grad, R.drawable.m8_grad,
-            R.drawable.m9_grad, R.drawable.m10_grad, R.drawable.m11_grad, R.drawable.m12_grad,
-            R.drawable.m13_grad, R.drawable.m14_grad, R.drawable.m15_grad, R.drawable.m16_grad,
-            R.drawable.m17_grad, R.drawable.m18_grad, R.drawable.m19_grad, R.drawable.m20_grad, R.drawable.m21_grad);
+    List<Integer>images_grad = new ArrayList<>();
 
-    List<Integer> images_bir = Arrays.asList(R.drawable.m1_bir, R.drawable.m2_bir, R.drawable.m3_bir, R.drawable.m4_bir,
-            R.drawable.m5_bir, R.drawable.m6_bir, R.drawable.m7_bir, R.drawable.m8_bir, R.drawable.m9_bir,
-            R.drawable.m10_bir, R.drawable.m11_bir, R.drawable.m12_bir, R.drawable.m13_bir, R.drawable.m14_bir,
-            R.drawable.m15_bir, R.drawable.m16_bir, R.drawable.m17_bir, R.drawable.m18_bir, R.drawable.m19_bir,
-            R.drawable.m20_bir, R.drawable.m21_bir);
+    List<Integer> images_bir= new ArrayList<>();;
 
-    List<Integer>images_hal = Arrays.asList(R.drawable.m1_hal, R.drawable.m2_hal, R.drawable.m3_hal, R.drawable.m4_hal,
-            R.drawable.m5_hal, R.drawable.m16_hal, R.drawable.m7_hal, R.drawable.m8_hal, R.drawable.m9_hal,
-            R.drawable.m10_hal, R.drawable.m11_hal, R.drawable.m12_hal, R.drawable.m13_hal, R.drawable.m14_hal,
-            R.drawable.m15_hal, R.drawable.m16_hal, R.drawable.m17_hal, R.drawable.m18_hal, R.drawable.m19_hal,
-            R.drawable.m20_hal, R.drawable.m21_hal);
+    List<Integer>images_hal= new ArrayList<>();;
 
-    List<Integer>images_lal = Arrays.asList(R.drawable.m1_lal, R.drawable.m2_lal, R.drawable.m3_lal, R.drawable.m4_lal,
-            R.drawable.m5_lal, R.drawable.m6_lal, R.drawable.m7_lal, R.drawable.m8_lal, R.drawable.m9_lal,
-            R.drawable.m10_lal, R.drawable.m11_lal, R.drawable.m12_lal, R.drawable.m13_lal, R.drawable.m14_lal,
-            R.drawable.m15_lal, R.drawable.m16_lal, R.drawable.m17_lal, R.drawable.m18_lal, R.drawable.m19_lal,
-            R.drawable.m20_lal, R.drawable.m21_lal);
+    List<Integer>images_lal= new ArrayList<>();;
+
 
 
     @Override
@@ -71,7 +56,16 @@ public class ImageGridActivity extends AppCompatActivity {
         bindService(intent, connection, Context.BIND_AUTO_CREATE);
         setContentView(binding.getRoot());
 
-        gridView = findViewById(R.id.gridView);
+        TextView topLabel = (TextView) findViewById(R.id.eventName);
+        topLabel.setText(getIntent().getStringExtra("eventName"));
+
+        for(int i =1 ; i<21 ; i++)
+        {
+            images_grad.add(getResources().getIdentifier("m" + i +"_grad", "drawable", getPackageName()));
+            images_bir.add(getResources().getIdentifier("m" + i +"_bir", "drawable", getPackageName()));
+            images_hal.add(getResources().getIdentifier("m" + i +"_hal", "drawable", getPackageName()));
+            images_lal.add(getResources().getIdentifier("m" + i +"_lal", "drawable", getPackageName()));
+        }
 
         switch(getIntent().getStringExtra("eventName"))
         {
@@ -84,6 +78,8 @@ public class ImageGridActivity extends AppCompatActivity {
             case "Lakers Game":images_selected = images_lal;
                 break;
         }
+
+        gridView = findViewById(R.id.gridView);
 
 
 
@@ -117,11 +113,42 @@ public class ImageGridActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                imageChooser();
             }
         });
 
 
 
+    }
+
+    void imageChooser() {
+
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), 200);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(getIntent().getStringExtra("eventName"))
+        {
+            case "Graduation":images_selected.add(R.drawable.m3_grad);
+                break;
+            case "Halloween":images_selected.add(R.drawable.m3_hal);
+                break;
+            case "Birthday":images_selected.add(R.drawable.m3_bir);
+                break;
+            case "Lakers Game":images_selected.add(R.drawable.m3_lal);
+                break;
+        }
+        customAdapter.notifyDataSetChanged();
     }
 
 
@@ -178,7 +205,6 @@ public class ImageGridActivity extends AppCompatActivity {
             AccountService.AccountBinder binder = (AccountService.AccountBinder) service;
             mService = binder.getService();
             mBound = true;
-            generateInitialView();
         }
 
         /**
@@ -195,15 +221,6 @@ public class ImageGridActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if(mBound) generateInitialView();
     }
-
-    private void  generateInitialView()
-    {
-        TextView topLabel = (TextView) findViewById(R.id.eventName);
-        topLabel.setText(getIntent().getStringExtra("eventName"));
-
-    }
-
 
 }
