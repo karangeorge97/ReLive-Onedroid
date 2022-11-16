c70c0123a3f24e493c14d13571509f0a8bcfb9da
karangeorge97 <karan9@illinois.edu>
2022-11-15 19:52:50 -0600

    Boilerplate setup for create event

--- app/src/main/java/com/onedroid/relive/CreateEvent.java
+++ app/src/main/java/com/onedroid/relive/CreateEvent.java
@@ -2,13 +2,87 @@ package com.onedroid.relive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
+
+import com.onedroid.relive.databinding.ActivityCreateEventBinding;
+import com.onedroid.relive.databinding.ActivityMainBinding;
+import com.onedroid.relive.model.Event;
+import com.onedroid.relive.service.AccountService;
 
 public class CreateEvent extends AppCompatActivity {
+
+
+    private ActivityCreateEventBinding binding;
+    AccountService mService;
+    boolean mBound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+        binding = ActivityCreateEventBinding.inflate(getLayoutInflater());
+        Intent intent = new Intent(this, AccountService.class);
+        intent.putExtra("username", getIntent().getStringExtra("username"));
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+        setContentView(binding.getRoot());
+
+        Button createEvent = findViewById(R.id.createEvent);
+        createEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Event event = new Event("","","");
+                try {
+                    mService.addEvent(event);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+
+
+    /**
+     * Initialize ServiceConnection.
+     */
+    private ServiceConnection connection = new ServiceConnection() {
+        /**
+         * Override onServiceConnected
+         * get service from binder and set bound to be true and then generate cities.
+         * @param componentName
+         * @param service
+         */
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            AccountService.AccountBinder binder = (AccountService.AccountBinder) service;
+            mService = binder.getService();
+            mBound = true;
+
+        }
+
+        /**
+         * Override onServiceDiconnected.
+         * Set bound to be false.
+         * @param componentName
+         */
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBound = false;
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mBound);
     }
 }
